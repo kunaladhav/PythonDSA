7e42522316017089ef0c23ac9d18ac3869d6139e
kunal adhav <kunal1603.adhav@gmail.com>
2024-01-21 22:34:14 +0530

    Section 1 Complete

--- pythonDSA.py
+++ pythonDSA.py
@@ -1,6 +1,42 @@
 # -----------------------------------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------
 
-#                      Python DSA Course Practice Rough Sheet
+#                              Python DSA Course
 
 # -----------------------------------------------------------------------------------
+
+def print_items(n):
+
+    for i in range(n):
+        for j in range(n):
+            return (i, j)
+
+    for k in range(n):
+        return (k)
+
+
+# -----------------------------------------------------------------------------------
+
+#                               Classes
+
+# -----------------------------------------------------------------------------------
+
+class Cookie:
+    def __init__(self, color):
+        self.color = color
+
+    def get_color(self):
+        return self.color
+
+    def set_color(self, color):
+        self.color = color
+
+
+cookie_1 = Cookie("green")
+cookie_2 = Cookie("blue")
+
+cookie_1.set_color("Violet")
+cookie_2.set_color("Yellow")
+
+print("The color of the Cookie 1 is", cookie_1.get_color())
+print("The color of the Cookie 2 is", cookie_2.get_color())
