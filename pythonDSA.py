712fd3af0ea9c957f0e5ac69b94d4537358d5263
Kunal Adhav <kunal1603.adhav@gmail.com>
2024-07-10 15:47:46 +0530

    practice problems

--- pythonDSA.py
+++ pythonDSA.py
@@ -5,14 +5,14 @@
 
 # -----------------------------------------------------------------------------------
 
-def print_items(n):
+# def print_items(n):
 
-    for i in range(n):
-        for j in range(n):
-            return (i, j)
+#     for i in range(n):
+#         for j in range(n):
+#             return (i, j)
 
-    for k in range(n):
-        return (k)
+#     for k in range(n):
+#         return (k)
 
 
 # -----------------------------------------------------------------------------------
@@ -21,22 +21,22 @@ def print_items(n):
 
 # -----------------------------------------------------------------------------------
 
-class Cookie:
-    def __init__(self, color):
-        self.color = color
+# class Cookie:
+#     def __init__(self, color):
+#         self.color = color
 
-    def get_color(self):
-        return self.color
+#     def get_color(self):
+#         return self.color
 
-    def set_color(self, color):
-        self.color = color
+#     def set_color(self, color):
+#         self.color = color
 
 
-cookie_1 = Cookie("green")
-cookie_2 = Cookie("blue")
+# cookie_1 = Cookie("green")
+# cookie_2 = Cookie("blue")
 
-cookie_1.set_color("Violet")
-cookie_2.set_color("Yellow")
+# cookie_1.set_color("Violet")
+# cookie_2.set_color("Yellow")
 
-print("The color of the Cookie 1 is", cookie_1.get_color())
-print("The color of the Cookie 2 is", cookie_2.get_color())
+# print("The color of the Cookie 1 is", cookie_1.get_color())
+# print("The color of the Cookie 2 is", cookie_2.get_color())
